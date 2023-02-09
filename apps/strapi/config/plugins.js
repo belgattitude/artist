6c33610ffdb99213fae22284ab4738aa75c16072
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-02-09 23:27:57 +0100

    feat: wip

--- apps/strapi/config/plugins.js
+++ apps/strapi/config/plugins.js
@@ -2,9 +2,10 @@ module.exports = ({ env }) => ({
   upload: {
     config: {
       sizeLimit: 500 * 1024 * 1024, // 512mb in bytes
+      /*
       // https://docs.strapi.io/developer-docs/latest/plugins/upload.html#configuration
       breakpoints: {
-        // As we rely on transformation from externl services (ie vercel/image)
+        // As we rely on transformation from external services (ie vercel/image)
         // no need to create multiple sizes that will have to be backup'd.
         // Warning when changing this, older images won't have thumbnails.
         // xlarge: 1920,
@@ -12,7 +13,7 @@ module.exports = ({ env }) => ({
         // medium: 750,
         // small: 500,
         xsmall: 64,
-      },
+      },*/
     },
   },
   graphql: {
