6bf1f71a649c839256e3f784f003a8b2784f7f97
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-02-20 12:51:16 +0100

    feat: type mdx components

--- apps/web/mdx-components.tsx
+++ apps/web/mdx-components.tsx
@@ -1,8 +1,6 @@
-// This file is required to use MDX in `app` directory.
-import type { ComponentType } from 'react';
-export function useMDXComponents(components: {
-  [component: string]: ComponentType;
-}) {
+import type { MDXComponents } from 'mdx/types';
+
+export function useMDXComponents(components: MDXComponents) {
   return {
     // Allows customizing built-in components, e.g. to add styling.
     // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
