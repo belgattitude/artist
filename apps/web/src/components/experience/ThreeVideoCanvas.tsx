97b5c3dcb97157e950a724b2dfbf00cd4036f474
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-03-02 13:54:08 +0100

    feat: small wip

--- apps/web/src/components/experience/ThreeVideoCanvas.tsx
+++ apps/web/src/components/experience/ThreeVideoCanvas.tsx
@@ -2,42 +2,40 @@
 
 import { useAspect, useTexture, useVideoTexture } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import type { FC } from 'react';
 import React, { Suspense } from 'react';
 
-// const img = 'https://media.failwell.be/pictures/butoh-picture.jpg';
-const img = 'https://media.failwell.be/mr/garage29/garage29-jeanne.jpg';
-// const videoUrl = 'https://media.failwell.be/dark.mp4';
-// const videoUrl = 'https://media.failwell.be/mr/garage29/garage29-background.mp4';
-const videoUrl =
-  'https://media.failwell.be/bubble/bubble_dance_11th_april_2021.mp4';
-
-function Scene() {
+const Scene: FC<{ imgUrl: string; videoUrl: string }> = (props) => {
   // const size = useAspect(1800, 1000);
+  const { videoUrl, imgUrl } = props;
   const size = useAspect(1800, 1000);
   return (
     <mesh scale={size}>
       <planeGeometry />
-      <Suspense fallback={<FallbackMaterial url={img} />}>
+      <Suspense fallback={<FallbackMaterial url={imgUrl} />}>
         <VideoMaterial url={videoUrl} />
       </Suspense>
     </mesh>
   );
-}
+};
 
-function VideoMaterial({ url }: { url: string }) {
+const VideoMaterial: FC<{ url: string }> = ({ url }) => {
   const texture = useVideoTexture(url, {
     autoplay: true,
     playbackRate: 1,
   });
   return <meshBasicMaterial map={texture} toneMapped={false} />;
-}
+};
 
 function FallbackMaterial({ url }: { url: string }) {
   const texture = useTexture(url);
   return <meshBasicMaterial map={texture} toneMapped={false} />;
 }
 
-export const ThreeVideoCanvas = () => {
+export const ThreeVideoCanvas: FC<{ imgUrl: string; videoUrl: string }> = (
+  props
+) => {
+  const { videoUrl, imgUrl } = props;
   return (
     <Canvas
       orthographic
@@ -56,7 +54,7 @@ export const ThreeVideoCanvas = () => {
       }}
       resize={{ scroll: true, debounce: { scroll: 50, resize: 0 } }}
     >
-      <Scene />
+      <Scene imgUrl={imgUrl} videoUrl={videoUrl} />
     </Canvas>
   );
 };
--- apps/web/src/components/experience/experience-page.tsx
+++ apps/web/src/components/experience/experience-page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { FC } from 'react';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import type { Experience } from '@/components/experience/experience-overlay';
 import { ExperienceOverlay } from '@/components/experience/experience-overlay';
 import { ThreeVideoCanvas } from '@/components/experience/ThreeVideoCanvas';
@@ -10,15 +10,25 @@ type Props = {
   experiences: Experience[];
 };
 
+// const img = 'https://media.failwell.be/pictures/butoh-picture.jpg';
+const img = 'https://media.failwell.be/mr/garage29/garage29-jeanne.jpg';
+// const videoUrl = 'https://media.failwell.be/dark.mp4';
+const videoUrl =
+  'https://media.failwell.be/mr/garage29/garage29-background.mp4';
+// const videoUrl =
+//  'https://media.failwell.be/bubble/bubble_dance_11th_april_2021.mp4';
+
 export const ExperiencePage: FC<Props> = (props) => {
   const { experiences } = props;
   const overlay = useRef();
   // const caption = useRef<HTMLSpanElement>();
   const scroll = useRef<number>(0);
 
+  const [video, setVideo] = useState<string>(videoUrl);
+
   return (
     <>
-      <ThreeVideoCanvas />
+      <ThreeVideoCanvas videoUrl={video} imgUrl={img} />
       <div>
         <ExperienceOverlay
           experiences={experiences}
