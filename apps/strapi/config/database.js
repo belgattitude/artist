d7e217fadcdf0654ced1be562aab953f0704dc05
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-03-03 23:47:10 +0100

    feat: small wip

--- apps/strapi/config/database.js
+++ apps/strapi/config/database.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
+  let dbFile = env('DATABASE_FILENAME');
+  if (!dbFile.startsWith('/')) {
+    dbFile = path.join(__dirname, '..', dbFile);
+  }
   return {
     connection: {
       client: 'sqlite',
       connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME')),
+        filename: dbFile,
       },
       useNullAsDefault: true,
     },
