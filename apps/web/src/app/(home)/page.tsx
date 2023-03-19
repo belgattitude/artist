c1f6e63ade51072e7820b4e4340789214e592891
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-03-20 00:02:41 +0100

    feat: small wip

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -10,13 +10,13 @@ export const metadata: Metadata = {
 const HomePage: FC = () => {
   const videoUrl = 'https://media.failwell.be/dark.mp4';
   return (
-    <div className={'border-1 container mx-auto flex flex-col gap-5 p-5'}>
+    <div className={'container mx-auto flex flex-col gap-5 p-5'}>
       <div className={'dot1 mt-5 text-white'}>
         <h1>Sébastien Vanvelthem</h1>
         <p>Dance | Instant composition | Improvisation</p>
         <p>New website coming soon</p>
       </div>
-      <div className={'grow-1 aspect-[2/1] w-full bg-black'}>
+      <div className={'mb-[50px] flex flex-col gap-5 bg-black md:flex-row'}>
         <VideoBackground
           sources={[
             {
@@ -28,6 +28,32 @@ const HomePage: FC = () => {
           playbackRate={0.3}
           autoPlay={true}
         />
+        <VideoBackground
+          sources={[
+            {
+              type: 'video/mp4',
+              // url: 'https://media.failwell.be/roz/roz-markten-trio.m4v',
+              // url: 'https://media.failwell.be/roz/Roz2.mp4',
+              url: 'https://media.failwell.be/bubble/bubble_dance_11th_april_2021.mp4',
+              // url: 'https://media.failwell.be/mr/garage29/garage29-background.mp4',
+            },
+          ]}
+          playbackRate={1.0}
+          autoPlay={true}
+        />
+        {/*
+        <VideoBackground
+          sources={[
+            {
+              type: 'video/mp4',
+              // url: 'https://media.failwell.be/roz/roz-markten-trio.m4v',
+              url: 'https://media.failwell.be/citla/citlali-full-graded.mp4',
+            },
+          ]}
+          playbackRate={0.7}
+          autoPlay={true}
+        />
+        */}
       </div>
     </div>
   );
--- apps/web/src/components/video/VideoBackground.tsx
+++ apps/web/src/components/video/VideoBackground.tsx
@@ -2,14 +2,21 @@
 
 import type { FC } from 'react';
 import { useCallback } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 type Props = {
+  className?: string;
   sources: Array<{ url: string; type: string }>;
   playbackRate: number;
   autoPlay: boolean;
 };
 export const VideoBackground: FC<Props> = (props) => {
-  const { sources, playbackRate = 1.0, autoPlay = true } = props;
+  const {
+    sources,
+    playbackRate = 1.0,
+    autoPlay = true,
+    className = '',
+  } = props;
 
   const videoRef = useCallback(
     (node: HTMLVideoElement) => {
@@ -22,11 +29,12 @@ export const VideoBackground: FC<Props> = (props) => {
 
   return (
     <div
-      className={
-        'm-5 flex w-[400px] flex-col border p-5 shadow-amber-900 drop-shadow-2xl'
-      }
+      className={twMerge(
+        'relative flex flex-col flex-grow bg-black drop-shadow pt-[20px]',
+        className
+      )}
     >
-      <div className={''}>
+      <div>
         <video
           className={
             'absolute top-0 left-0 -z-10 block h-full w-full overflow-hidden bg-black object-cover'
@@ -42,26 +50,32 @@ export const VideoBackground: FC<Props> = (props) => {
           })}
         </video>
       </div>
-      <div className={'text-white'}>
-        <h1 className={'text-4xl font-extrabold'}>Hello world</h1>
-        <p>
-          Iqlksqdjf lqsdjf qqs dflkjqsd f qs dflqsjdf lqsd jf qs dflsqdjf qs df
-          qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl
-          qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl
-          qsdlkfj sqdf lkq jdfl
-        </p>
-        <p>
-          Iqlksqdjf lqsdjf qqs dflkjqsd f qs dflqsjdf lqsd jf qs dflsqdjf qs df
-          qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl
-          qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl
-          qsdlkfj sqdf lkq jdfl
-        </p>
-        <p>
-          Iqlksqdjf lqsdjf qqs dflkjqsd f qs dflqsjdf lqsd jf qs dflsqdjf qs df
-          qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl
-          qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl
-          qsdlkfj sqdf lkq jdfl
-        </p>
+      <div
+        className={
+          'mb-[50px] mr-[20px] max-w-[400px] bg-black/40 p-[65px] tracking-wide text-[#a0a0a0]'
+        }
+      >
+        <h1 className={'mb-5  text-6xl font-thin text-white'}>Casual</h1>
+        <div className={''}>
+          <p>
+            Iqlksqdjf lqsdjf qqs dflkjqsd f qs dflqsjdf lqsd jf qs dflsqdjf qs
+            df qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl
+            qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl
+            qsdlkfj sqdf lkq jdfl
+          </p>
+          <p>
+            Iqlksqdjf lqsdjf qqs dflkjqsd f qs dflqsjdf lqsd jf qs dflsqdjf qs
+            df qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl
+            qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl
+            qsdlkfj sqdf lkq jdfl
+          </p>
+          <p>
+            Iqlksqdjf lqsdjf qqs dflkjqsd f qs dflqsjdf lqsd jf qs dflsqdjf qs
+            df qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl
+            qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl qsdlkfj sqdf lkq jdfl
+            qsdlkfj sqdf lkq jdfl
+          </p>
+        </div>
       </div>
     </div>
   );
