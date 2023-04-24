45f8c8bacece8fa1f815c332dcf9927b71e9a796
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-04-24 16:55:09 +0200

    feat: small wip

--- apps/web/src/app/video/layout.tsx
+++ apps/web/src/app/video/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { ReactNode } from 'react';
 
 export const metadata = {};
@@ -5,7 +6,10 @@ export const metadata = {};
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
