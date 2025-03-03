38d8241da14f2c5c23e11b4cbb4e01a46dbd69f1
Sébastien Vanvelthem <belgattitude@gmail.com>
2025-03-03 14:30:36 +0100

    feat: next

--- apps/web/src/config/bunny.server.config.ts
+++ apps/web/src/config/bunny.server.config.ts
@@ -1,11 +1,14 @@
-import { serverEnv } from '@/env/server.env.mjs';
-
 export const bunnyStreamServerConfig = {
   embros: {
     getAccessKey: () => {
+      /*
       return typeof window === 'undefined'
         ? ''
         : (serverEnv.BUNNY_STREAM_ACCESS_KEY ?? '');
+      */
+      return typeof window === 'undefined'
+        ? ''
+        : (process.env.BUNNY_STREAM_ACCESS_KEY ?? '');
     },
   },
 } as const;
