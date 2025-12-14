b439a4c8737236a29e4e8f096f2951d42f5bcb31
Sebastien Vanvelthem <belgattitude@gmail.com>
2025-12-14 20:44:54 +0100

    chore: to latest

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -30,7 +30,6 @@ module.exports = {
     '@belgattitude/eslint-config-bases/regexp',
     '@belgattitude/eslint-config-bases/react',
     '@belgattitude/eslint-config-bases/react-query',
-    '@belgattitude/eslint-config-bases/storybook',
     // after next plugins to avoid parser errors
     // '@belgattitude/eslint-config-bases/mdx',
     // Apply prettier and disable incompatible rules
